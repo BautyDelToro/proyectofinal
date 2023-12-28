79d920bfe4a6bf0f1c4b1327b8b1bc2b2e99023e
BautyDelToro <deltorobautista@gmail.com>
2023-12-28 01:44:20 -0300

    boton terminado

--- app/Componentes/Imagentipo.jsx
+++ app/Componentes/Imagentipo.jsx
@@ -2,23 +2,35 @@
 import '../pokemones/pokemones.css'
 import { useState } from 'react';
 
-export default function Imagentipo ({titulo, imagen, accesibilidad, id}) {
+export default function Imagentipo ({titulo, imagen, accesibilidad,idd}) {
     
     const [boton, setBoton] = useState(0);
-    function botonusado(id){
+    function botonusado(){
         if(boton===0){
             console.log("ola");
             setBoton(1);
-            console.log({id})
+            document.getElementById("botonpoke"+idd).classList.remove('botondespliegue')
+            document.getElementById("botonpoke"+idd).classList.add('botonEncendido');
+
+            document.getElementById("flecha"+idd).classList.add('flechaDerecha');
         }
         else if (boton===1){
             console.log("olu")
             setBoton(2);
-            console.log({id})
+            document.getElementById("botonpoke"+idd).classList.remove('botonEncendido')
+            document.getElementById("botonpoke"+idd).classList.add('botonMovido')
+
+            document.getElementById("flecha"+idd).classList.remove('flechaDerecha')
+            document.getElementById("flecha"+idd).classList.add('flechaIzquierda');
         }
         else if(boton===2){
             console.log("popo")
             setBoton(1);
+            document.getElementById("botonpoke"+idd).classList.remove('botonMovido')
+            document.getElementById("botonpoke"+idd).classList.add('botonEncendido')
+
+            document.getElementById("flecha"+idd).classList.remove('flechaIzquierda')
+            document.getElementById("flecha"+idd).classList.add('flechaDerecha');
         }
     }
 
@@ -28,36 +40,12 @@ export default function Imagentipo ({titulo, imagen, accesibilidad, id}) {
                 <h3 className='titulopoke'>{titulo}</h3>
                 <img src={imagen} alt={accesibilidad} className="imagenlogotipo"/>
             </div>
-            
-            {(boton===0) &&
-            <button id='botonpoke' onClick={()=>{botonusado(id)}} className='botonDespliegue'> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-arrow-bar-right" viewBox="0 0 16 16">
+    
+            <button id={'botonpoke'+idd} onClick={botonusado} className='botonDespliegue'> 
+                <svg id={"flecha"+idd} className="flecha" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill='currentcolor' viewBox="0 0 16 16">
                  <path fillRule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
                 </svg> 
-            </button>  
-            }
-            {(boton===1) &&
-            <>
-                <button id='botonpoke' onClick={()=>{botonusado(id)}} className='botonEncendido'> 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="flechaDerecha" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
-                    </svg> 
-                </button>
-
-                
-            </>
-            }
-            {(boton===2) &&
-            <>
-                <button id='botonpoke' onClick={()=>{botonusado(id)}} className='botonMovido'> 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="flechaIzquierda" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
-                    </svg> 
-                </button>  
-                
-
-            </>
-            }
+            </button>
         </div>
     )
 }
--- app/pokemones/page.jsx
+++ app/pokemones/page.jsx
@@ -9,15 +9,15 @@ export default function Pokemones (){
         <div className='divpokemones'>
             <h1>Pokemones Segun su tipo</h1>
             <div>
-                <Imagentipo titulo="Acero" imagen="/imagenes/Acero.png" accesibilidad="Logotipo Acero" id="steel"/>
-                <Imagentipo titulo="Dragón" imagen="/imagenes/Dragon.png" accesibilidad="Logotipo Dragón" id="dragon"/>
-                <Imagentipo titulo="Agua" imagen="/imagenes/Agua.png" accesibilidad="Logotipo Agua" id="water"/>
-                <Imagentipo titulo="Eléctrico" imagen="/imagenes/Electrico.png" accesibilidad="Logotipo Eléctrico" id="electric"/>
-                <Imagentipo titulo="Fantasma" imagen="/imagenes/Fantasma.png" accesibilidad="Logotipo Fantasma" id="ghost"/>
-                <Imagentipo titulo="Hada" imagen="/imagenes/Hada.png" accesibilidad="Logotipo Hada" id="fairy"/>
-                <Imagentipo titulo="Planta" imagen="/imagenes/Planta.png" accesibilidad="Logotipo Planta" id="grass"/>
-                <Imagentipo titulo="Psíquico" imagen="/imagenes/Psiquico.png" accesibilidad="Logotipo Psíquico" id="psychic"/>
-                <Imagentipo titulo="Siniestro" imagen="/imagenes/Siniestro.png" accesibilidad="Logotipo Siniestro" id="dark"/>
+                <Imagentipo titulo="Acero" imagen="/imagenes/Acero.png" accesibilidad="Logotipo Acero" idd="steel"/>
+                <Imagentipo titulo="Dragón" imagen="/imagenes/Dragon.png" accesibilidad="Logotipo Dragón" idd="dragon"/>
+                <Imagentipo titulo="Agua" imagen="/imagenes/Agua.png" accesibilidad="Logotipo Agua" idd="water"/>
+                <Imagentipo titulo="Eléctrico" imagen="/imagenes/Electrico.png" accesibilidad="Logotipo Eléctrico" idd="electric"/>
+                <Imagentipo titulo="Fantasma" imagen="/imagenes/Fantasma.png" accesibilidad="Logotipo Fantasma" idd="ghost"/>
+                <Imagentipo titulo="Hada" imagen="/imagenes/Hada.png" accesibilidad="Logotipo Hada" idd="fairy"/>
+                <Imagentipo titulo="Planta" imagen="/imagenes/Planta.png" accesibilidad="Logotipo Planta" idd="grass"/>
+                <Imagentipo titulo="Psíquico" imagen="/imagenes/Psiquico.png" accesibilidad="Logotipo Psíquico" idd="psychic"/>
+                <Imagentipo titulo="Siniestro" imagen="/imagenes/Siniestro.png" accesibilidad="Logotipo Siniestro" idd="dark"/>
 
             </div>
         </div>
